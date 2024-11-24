918a02d376a3e18276c44c607e6cf6b7dc7e3026
samleticias <sammyaleticia09@gmail.com>
2024-11-24 15:45:46 -0300

    feat: create a method to register vacancies

---
+++ src/main/java/com/example/apprh/controllers/VagaController.java
@@ -0,0 +1,43 @@
+package com.example.apprh.controllers;
+
+import com.example.apprh.models.Vaga;
+import com.example.apprh.repository.CandidatoRepository;
+import com.example.apprh.repository.VagaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+
+@Controller
+public class VagaController {
+
+    @Autowired
+    private VagaRepository vr;
+
+    @Autowired
+    private CandidatoRepository cr;
+
+    // método GET que chama o formulário que cadastra vaga
+    @RequestMapping("/cadastrarVaga")
+    public String form() {
+        return "vaga/form-vaga";
+    }
+
+    // método POST que cadastra a vaga
+    @RequestMapping(value = "/cadastrarVaga", method = RequestMethod.POST)
+    public String form(@Valid Vaga vaga, BindingResult result, RedirectAttributes attributes) {
+
+        if (result.hasErrors()) {
+            attributes.addFlashAttribute("mensagem", "Verifique os campos...");
+            return "redirect:/cadastrarVaga";
+        }
+
+        vr.save(vaga);
+        attributes.addFlashAttribute("mensagem", "Vaga cadastrada com sucesso!");
+        return "redirect:/cadastrarVaga";
+    }
+}
